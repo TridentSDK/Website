45cf2d96f704e6e1aa63ce2c0db63b7945170470
Vilsol <vilsol2000@gmail.com>
2016-07-20 00:15:56 +0300

    Add microdata to articles

--- resources/views/home/news/article.blade.php
+++ resources/views/home/news/article.blade.php
@@ -1,10 +1,10 @@
-<div class="panel panel-default plugin-sidebar">
+<div class="panel panel-default plugin-sidebar" itemscope itemtype="http://schema.org/Article">
     <div class="panel-heading">
-        <h3 class="panel-title pull-left"><a href="/n/{{ $article->id }}">{{ $article->title }}</a></h3>
-        <span class="pull-right" title="{{ $article->created_at }}">{{ $article->created_at->diffForHumans() }}</span>
+        <h3 class="panel-title pull-left" itemprop="name"><a href="/n/{{ $article->id }}">{{ $article->title }}</a></h3>
+        <time datetime="{{ $article->created_at }}" itemprop="datePublished" class="pull-right" title="{{ $article->created_at }}">{{ $article->created_at->diffForHumans() }}</time>
         <div class="clearfix"></div>
     </div>
-    <div class="panel-body">
+    <div class="panel-body" itemprop="text">
         {!! $article->text !!}
         <div class="clearfix"></div>
     </div>
