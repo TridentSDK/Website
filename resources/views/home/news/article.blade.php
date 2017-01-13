b1e10783f1a537ec52cee0f2ee015b332dbfc4ee
Vilsol <vilsol2000@gmail.com>
2017-01-13 04:12:29 +0000

    Add necessary meta tags

--- resources/views/home/news/article.blade.php
+++ resources/views/home/news/article.blade.php
@@ -1,4 +1,10 @@
 <div class="panel panel-default plugin-sidebar" itemscope itemtype="http://schema.org/Article">
+    <meta itemprop="author" content="TridentSDK Team" />
+    <meta itemprop="headline" content="{{ $article->title }}" />
+    <meta itemprop="publisher" content="TridentSDK" />
+    <meta itemprop="dateModified" content="{{ $article->updated_at }}" />
+    <meta itemprop="mainEntityOfPage" content="{{ env("APP_URL") }}" />
+
     <div class="panel-heading">
         <h3 class="panel-title pull-left" itemprop="name"><a href="/n/{{ $article->id }}">{{ $article->title }}</a></h3>
         <time datetime="{{ $article->created_at }}" itemprop="datePublished" class="pull-right" title="{{ $article->created_at }}">{{ $article->created_at->diffForHumans() }}</time>
