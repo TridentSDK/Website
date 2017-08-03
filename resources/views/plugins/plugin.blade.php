a0a67598a1425d9f36c2072a5f22d66696af6b43
Vilsol <vilsol2000@gmail.com>
2017-08-04 02:14:19 +0300

    Hide things if not applicable

--- resources/views/plugins/plugin.blade.php
+++ resources/views/plugins/plugin.blade.php
@@ -21,42 +21,46 @@
                     <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> <strong>Views:</strong> {{ $plugin->views }}<br>
                     <span class="glyphicon glyphicon-cloud-download" aria-hidden="true"></span> <strong>Downloads:</strong> {{ $plugin->downloads }}<br>
                     <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> <strong>Favourites:</strong> {{ $plugin->favourites }}<br>
-                    <span class="glyphicon glyphicon-fire" aria-hidden="true"></span> <strong>Latest Version:</strong> {{ $plugin->latestversion }}<br>
+                    @if($plugin->latestversion != null)
+                        <span class="glyphicon glyphicon-fire" aria-hidden="true"></span> <strong>Latest Version:</strong> {{ $plugin->latestversion }}<br>
+                    @endif
                     <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> <strong>Created:</strong> <time datetime="{{ $plugin->created_at }}" itemprop="datePublished" title="{{ $plugin->created_at }}">{{ $plugin->created_at->diffForHumans() }}</time><br>
                     <span class="glyphicon glyphicon-list" aria-hidden="true"></span> <strong>License:</strong> <a href="{{ $plugin->getLicense()[1] }}">{{ $plugin->getLicense()[0] }}</a><br>
                 </div>
             </div>
-            <div class="panel panel-success">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Installation</h3>
-                </div>
-                <div class="panel-body tabbed">
-                    <ul class="nav nav-tabs" role="tablist">
-                        <li role="presentation" class="active"><a href="#owner" aria-controls="home" role="tab" data-toggle="tab">Server</a></li>
-                        <li role="presentation"><a href="#maven" aria-controls="profile" role="tab" data-toggle="tab">Maven</a></li>
-                        <li role="presentation"><a href="#gradle" aria-controls="profile" role="tab" data-toggle="tab">Gradle</a></li>
-                        <li role="presentation"><a href="#module" aria-controls="profile" role="tab" data-toggle="tab">Module</a></li>
-                    </ul>
-                    <div class="tab-content">
-                        <div role="tabpanel" class="tab-pane preformatted active" id="owner">
-                            <span style="color: #66D9EF">/trd</span> <span style="color: #FD971F">i</span> <span style="color: #A6E22E">{{ $plugin->getSpaceWithArtifact() }}</span> <span style="color: #F92672">{{ $plugin->latestversion }}</span>
-                        </div>
-                        <div role="tabpanel" class="tab-pane preformatted" id="maven">
-                            &lt;<span style="color: #F92672">dependency</span>&gt;<br>
-                            &nbsp;&nbsp;&lt;<span style="color: #F92672">groupId</span>&gt;trd.{{ $plugin->getSpace()->name }}&lt;<span style="color: #F92672">/groupId</span>&gt;<br>
-                            &nbsp;&nbsp;&lt;<span style="color: #F92672">artifactId</span>&gt;{{ $plugin->artifact }}&lt;<span style="color: #F92672">/artifactId</span>&gt;<br>
-                            &nbsp;&nbsp;&lt;<span style="color: #F92672">version</span>&gt;{{ $plugin->latestversion }}&lt;<span style="color: #F92672">/version</span>&gt;<br>
-                            &lt;/<span style="color: #F92672">dependency</span>&gt;
-                        </div>
-                        <div role="tabpanel" class="tab-pane preformatted" id="gradle">
-                            provided <span style="color: #E6DB74">"trd.{{ $plugin->getSpace()->name }}:{{ $plugin->artifact }}:{{ $plugin->latestversion }}"</span>
-                        </div>
-                        <div role="tabpanel" class="tab-pane preformatted" id="module">
-                            @<span style="color: #66D9EF">DependsOn</span>(id = <span style="color: #E6DB74">"{{ $plugin->getSpaceWithArtifact() }}"</span>, version = <span style="color: #E6DB74">"{{ $plugin->latestversion }}"</span>)
+            @if($plugin->latestversion != null)
+                <div class="panel panel-success">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Installation</h3>
+                    </div>
+                    <div class="panel-body tabbed">
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li role="presentation" class="active"><a href="#owner" aria-controls="home" role="tab" data-toggle="tab">Server</a></li>
+                            <li role="presentation"><a href="#maven" aria-controls="profile" role="tab" data-toggle="tab">Maven</a></li>
+                            <li role="presentation"><a href="#gradle" aria-controls="profile" role="tab" data-toggle="tab">Gradle</a></li>
+                            <li role="presentation"><a href="#module" aria-controls="profile" role="tab" data-toggle="tab">Module</a></li>
+                        </ul>
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane preformatted active" id="owner">
+                                <span style="color: #66D9EF">/trd</span> <span style="color: #FD971F">i</span> <span style="color: #A6E22E">{{ $plugin->getSpaceWithArtifact() }}</span> <span style="color: #F92672">{{ $plugin->latestversion }}</span>
+                            </div>
+                            <div role="tabpanel" class="tab-pane preformatted" id="maven">
+                                &lt;<span style="color: #F92672">dependency</span>&gt;<br>
+                                &nbsp;&nbsp;&lt;<span style="color: #F92672">groupId</span>&gt;trd.{{ $plugin->getSpace()->name }}&lt;<span style="color: #F92672">/groupId</span>&gt;<br>
+                                &nbsp;&nbsp;&lt;<span style="color: #F92672">artifactId</span>&gt;{{ $plugin->artifact }}&lt;<span style="color: #F92672">/artifactId</span>&gt;<br>
+                                &nbsp;&nbsp;&lt;<span style="color: #F92672">version</span>&gt;{{ $plugin->latestversion }}&lt;<span style="color: #F92672">/version</span>&gt;<br>
+                                &lt;/<span style="color: #F92672">dependency</span>&gt;
+                            </div>
+                            <div role="tabpanel" class="tab-pane preformatted" id="gradle">
+                                provided <span style="color: #E6DB74">"trd.{{ $plugin->getSpace()->name }}:{{ $plugin->artifact }}:{{ $plugin->latestversion }}"</span>
+                            </div>
+                            <div role="tabpanel" class="tab-pane preformatted" id="module">
+                                @<span style="color: #66D9EF">DependsOn</span>(id = <span style="color: #E6DB74">"{{ $plugin->getSpaceWithArtifact() }}"</span>, version = <span style="color: #E6DB74">"{{ $plugin->latestversion }}"</span>)
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 
