9f02af28b616044cb61936a5322cc2cfb457741d
Vilsol <vilsol2000@gmail.com>
2017-08-08 02:39:31 +0300

    Fix name

--- database/migrations/2017_08_07_041555_allow_null_plugin_fields.php
+++ database/migrations/2017_08_07_041555_allow_null_plugin_fields.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AllowNullLatestVersion extends Migration
+class AllowNullPluginFields extends Migration
 {
     /**
      * Run the migrations.
