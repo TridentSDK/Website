6f83fd689668cf8cc25e9690cda81c5c65105905
Vilsol <vilsol2000@gmail.com>
2017-08-08 20:21:56 +0300

    Version download

--- resources/views/plugins/plugin.blade.php
+++ resources/views/plugins/plugin.blade.php
@@ -4,11 +4,47 @@
 
 @section('content')
 
+    <ol class="breadcrumb headcrab">
+        <li><a href="/plugins/">Plugins</a></li>
+        <li>{{ $plugin->name }}</li>
+    </ol>
+
     <div class="row plugin-page">
         <div class="col-md-8 col-md-8-5">
-            <div class="panel">
-                <div class="panel-body">
-                    {{ $plugin->fulldescription }}
+            <div class="panel panel-info-original">
+                <div class="panel-body tabbed">
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#description" role="tab" data-toggle="tab">Description</a></li>
+                        <li role="presentation"><a href="#version" role="tab" data-toggle="tab">Versions</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="description">
+                            {{ $plugin->fulldescription }}
+                        </div>
+                        <div role="tabpanel" class="tab-pane nopadding panel-margin-fixer" id="version">
+                            <div class="panel-group mb-0" id="versions" role="tablist" aria-multiselectable="true">
+                                @php($first = true)
+                                @php($space = $plugin->getSpace())
+                                @foreach(collect($plugin->versions())->sortByDesc("created_at") as $version)
+                                        <div class="panel panel-success straight-corner">
+                                            <div class="panel-heading straight-corner" role="tab" id="version-heading-{{ $version->id }}">
+                                                <h4 class="panel-title">
+                                                    <a role="button" data-toggle="collapse" data-parent="#versions" href="#version-{{ $version->id }}" aria-expanded="true" aria-controls="version-{{ $version->id }}"{{ $first ? "" : " class='collapsed'" }}>
+                                                        Version {{ $version->version }}
+                                                    </a>
+                                                </h4>
+                                            </div>
+                                            <div id="version-{{ $version->id }}" class="panel-collapse collapse{{ $first ? " in" : "" }}" role="tabpanel" aria-labelledby="version-heading-{{ $version->id }}">
+                                                <div class="panel-body">
+                                                    <a class="btn btn-success" href="{{ url(Storage::url("plugins/" . $space->name . "/" . $plugin->artifact . "/" . $version->filename)) }}">Download</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @php($first = false)
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -40,10 +76,10 @@
                     </div>
                     <div class="panel-body tabbed">
                         <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#owner" aria-controls="home" role="tab" data-toggle="tab">Server</a></li>
-                            <li role="presentation"><a href="#maven" aria-controls="profile" role="tab" data-toggle="tab">Maven</a></li>
-                            <li role="presentation"><a href="#gradle" aria-controls="profile" role="tab" data-toggle="tab">Gradle</a></li>
-                            <li role="presentation"><a href="#module" aria-controls="profile" role="tab" data-toggle="tab">Module</a></li>
+                            <li role="presentation" class="active"><a href="#owner" role="tab" data-toggle="tab">Server</a></li>
+                            <li role="presentation"><a href="#maven" role="tab" data-toggle="tab">Maven</a></li>
+                            <li role="presentation"><a href="#gradle" role="tab" data-toggle="tab">Gradle</a></li>
+                            <li role="presentation"><a href="#module" role="tab" data-toggle="tab">Module</a></li>
                         </ul>
                         <div class="tab-content">
                             <div role="tabpanel" class="tab-pane preformatted active" id="owner">
